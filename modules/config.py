d11a3ec880e205601f6fdb0c0dadda138590530b
ShailendraOP <91666183+ShailendraOP@users.noreply.github.com>
2022-05-25 08:14:41 +0530

    Add files via upload

--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,16 @@
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+    
+load_dotenv()
+que = {}
+admins = {}
 
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "900"))
+STRING_SESSION = getenv("STRING_SESSION", "")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1979178376").split()))
+aiohttpsession = aiohttp.ClientSession()
