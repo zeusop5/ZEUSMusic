cc43bad2b7347baae837d941029f40e48f4ae1ae
ShailendraOP <91666183+ShailendraOP@users.noreply.github.com>
2022-05-25 15:31:29 +0530

    Update __init__.py

--- modules/helpers/SQL/__init__.py
+++ modules/helpers/SQL/__init__.py
@@ -1,6 +1,8 @@
 
-from modules.config import MONGO_DB
 import motor.motor_asyncio
+
+MONGO_DB = "mongodb+srv://Zaid:Zaid@cluster0.4bszo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 mongo_dbb = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
 dbb = mongo_dbb["SPAMBOT"]
 SPAMBOT = 'SPAMBOT'
